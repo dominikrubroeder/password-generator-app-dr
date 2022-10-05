1af389c3266182b2abe1f82bee4a2874321ef6e5
Dominik <dominik.rubroeder@icloud.com>
2022-10-05 10:26:18 +0200

    Add svgs, create custom Checkbox component

---
+++ components/Checkbox.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import IconCheck from './svg/IconCheck';
+
+interface CheckboxProps {
+  checked: boolean;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({ checked }) => {
+  return (
+    <div
+      className={`w-5 h-5 border flex items-center justify-center transition ${
+        checked
+          ? 'border-app-green bg-app-green'
+          : 'border-white bg-transparent'
+      }`}
+    >
+      {checked && <IconCheck />}
+    </div>
+  );
+};
+
+export default Checkbox;
--- components/PasswordCard.tsx
+++ components/PasswordCard.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from './Checkbox';
+import IconArrowRight from './svg/IconArrowRight';
+import IconCopy from './svg/IconCopy';
 
 const PasswordCard: React.FC = () => {
+  const [includeUppercaseLetters, setIncludeUppercaseLetters] = useState(false);
+  const [includeLowercaseLetters, setIncludeLowercaseLetters] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+
   return (
     <div className="grid gap-4 max-w-[33.75rem]">
-      <header className="heading bg-app-gray-800 p-4">
+      <header className="flex justify-between items-center heading bg-app-gray-800 p-4">
         <input
           className="bg-transparent focus:outline-none"
           value="PTx1f5DaFX"
           readOnly
         />
+
+        <button>
+          <IconCopy />
+        </button>
       </header>
 
       <div className="grid gap-4 py-6 px-8 bg-app-gray-800">
@@ -20,31 +32,57 @@ const PasswordCard: React.FC = () => {
         <input type="range" className="w-full" />
 
         <ul className="grid gap-5">
-          <li className="flex items-center gap-6">
-            <input type="checkbox" />
+          <li
+            className="flex items-center gap-6 cursor-pointer"
+            onClick={() =>
+              setIncludeUppercaseLetters((previousState) => !previousState)
+            }
+          >
+            <Checkbox checked={includeUppercaseLetters} />
             Include Uppercase Letters
           </li>
-          <li className="flex items-center gap-6">
-            <input type="checkbox" />
+          <li
+            className="flex items-center gap-6 cursor-pointer"
+            onClick={() =>
+              setIncludeLowercaseLetters((previousState) => !previousState)
+            }
+          >
+            <Checkbox checked={includeLowercaseLetters} />
             Include Lowercase Letters
           </li>
-          <li className="flex items-center gap-6">
-            <input type="checkbox" />
+          <li
+            className="flex items-center gap-6 cursor-pointer"
+            onClick={() => setIncludeNumbers((previousState) => !previousState)}
+          >
+            <Checkbox checked={includeNumbers} />
             Include Numbers
           </li>
-          <li className="flex items-center gap-6">
-            <input type="checkbox" />
+          <li
+            className="flex items-center gap-6 cursor-pointer"
+            onClick={() => setIncludeSymbols((previousState) => !previousState)}
+          >
+            <Checkbox checked={includeSymbols} />
             Include Symbols
           </li>
         </ul>
 
         <div className="flex items-center justify-between px-6 py-4 bg-app-gray-900">
-          <h2>Strength</h2>
-          <div>...</div>
+          <h2 className="text-app-gray-500">Strength</h2>
+
+          <div className="grid grid-cols-2 gap-2">
+            <span className="uppercase">Medium</span>
+            <div className="grid grid-cols-4">
+              <span className="w-[.625rem] h-full border border-app-yellow bg-app-yellow"></span>
+              <span className="w-[.625rem] h-full border border-app-yellow bg-app-yellow"></span>
+              <span className="w-[.625rem] h-full border border-app-yellow bg-app-yellow"></span>
+              <span className="w-[.625rem] h-full border border-bg-white bg-transparent"></span>
+            </div>
+          </div>
         </div>
 
-        <button className="bg-app-green text-gray-800 uppercase p-4">
+        <button className="bg-app-green text-gray-800 uppercase p-4 flex items-center gap-2 justify-center">
           Generate
+          <IconArrowRight />
         </button>
       </div>
     </div>
